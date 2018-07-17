6d5809cc32706f0596c643d7a4b19152e5a5d9d0
Jona <jon_ra@hotmail.es>
2018-07-17 17:36:50 -0500

    añadir otros

---
+++ BirthdayGifts/Assets/Gift1/Scripts/ExtendedTracker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyAR;
+
+/* https://www.youtube.com/watch?v=7-2LMW26xoM&t=507s */
+public class ExtendedTracker : MonoBehaviour {
+    public GameObject Target;
+    public GameObject ContainerTarget;
+    public float timeActiveOnTargetLose = .5f;
+
+    bool detected = false;
+    float timer = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Target.activeSelf==true)
+        {
+            detected = true;
+            timer = 0f;
+        }
+        else
+        {
+            detected = false;
+        }
+
+        if (Target.activeSelf == false && timer < timeActiveOnTargetLose)
+        {
+            ContainerTarget.transform.SetParent(this.transform);
+            timer += Time.deltaTime;
+        }
+         else if(Target.activeSelf==false){
+            ContainerTarget.transform.SetParent(Target.transform);
+
+        }
+		
+	}
+}
