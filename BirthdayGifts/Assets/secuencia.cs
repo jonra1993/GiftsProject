d0fb58d7da3a9222d7788653630a5571047c33c6
Jona <jon_ra@hotmail.es>
2018-07-25 20:08:09 -0500

    letras y pastel

---
+++ BirthdayGifts/Assets/secuencia.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secuencia : MonoBehaviour {
+    GameObject cubo;
+    public float lapso = .5f;
+    private Animator animator;
+    float timer = 0;
+
+    // Use this for initialization
+    void Start () {
+        cubo = GameObject.Find("Cube");
+
+        if ((cubo != null) && (cubo.GetComponent<Animator>() != null))
+        {
+            animator = cubo.GetComponent<Animator>();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (timer < lapso)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            if (animator != null) animator.SetTrigger("regalo");
+            timer = 0;
+        }
+    }
+}
