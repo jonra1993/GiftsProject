c6868940f75ae4838f9318a83cb285be76ea7f54
Jona <jon_ra@hotmail.es>
2018-08-29 11:09:28 -0500

    drawer

--- BirthdayGifts/UsingQRCode/Assets/birthdayRober/Scripts/Extended.cs
+++ BirthdayGifts/UsingQRCode/Assets/birthdayRober/Scripts/Extended.cs
@@ -5,18 +5,20 @@ using EasyAR;
 
 /* https://www.youtube.com/watch?v=7-2LMW26xoM&t=507s */
 public class Extended : MonoBehaviour {
+    public GameObject Puntero;
     public GameObject Message;
     public GameObject Target;
     public GameObject ContainerTarget;
-    public float timeActiveOnTargetLose = .5f;
+    public float timeActiveOnTargetLose = .1f;
 
     bool detected = false;
     float timer = 0;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        ContainerTarget.transform.SetParent(Target.transform);
+        timer = timeActiveOnTargetLose+1;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -24,6 +26,7 @@ public class Extended : MonoBehaviour {
         {
             timer = 0f;
             Message.SetActive(false);
+            Puntero.SetActive(false);
         }
         else
         {
@@ -37,6 +40,7 @@ public class Extended : MonoBehaviour {
             {
                 ContainerTarget.transform.SetParent(Target.transform);
                 Message.SetActive(true);
+                Puntero.SetActive(true);
 
 
             }
---
+++ BirthdayGifts/UsingQRCode/Assets/birthdayRober/Scripts/drawer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawer : MonoBehaviour {
+
+    bool mem = false;
+    public GameObject Dra;
+    // Update is called once per frame
+    // Use this for initialization
+    void Start()
+    {
+        Dra.SetActive(false);
+    }
+
+    public void toggle () {
+
+        if (mem == false)
+        {
+            Dra.SetActive(true);
+            mem = true;
+        }
+        else
+        {
+            Dra.SetActive(false);
+            mem = false;
+
+        }
+    }
+}
